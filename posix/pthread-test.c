a8bbaca1994e59ad13b56e8f99ff0fccbe635b1e
Jordan Dixon <jordan.dixon211@gmail.com>
2019-06-07 14:10:16 +0100

    first commit

---
+++ posix/pthread-test.c
@@ -0,0 +1,208 @@
+#include "pthread-test.h"
+
+void _add_wait_list(Waiter **wait_list, Waiter *waiter)
+{
+  if (NULL == *wait_list) {
+    // Nothing in the list so add it directly.
+    // Update prev and next pointer to reference self
+    *wait_list = waiter;
+    waiter->next = waiter;
+    waiter->prev = waiter;
+  } else {
+    // Add after the last element
+    Waiter *first = *wait_list;
+    Waiter *last = (*wait_list)->prev;
+
+    // Update new entry
+    waiter->next = first;
+    waiter->prev = last;
+
+    // Insert into the list
+    first->prev = waiter;
+    last->next = waiter;
+  }
+  waiter->in_list = true;
+}
+
+void _remove_wait_list(Waiter **wait_list, Waiter *waiter)
+{
+  Waiter *prev = waiter->prev;
+  Waiter *next = waiter->next;
+
+  // Remove from list
+  prev->next = waiter->next;
+  next->prev = waiter->prev;
+  *wait_list = waiter->next;
+
+  if (*wait_list == waiter) {
+    // This was the last element in the list
+    *wait_list = NULL;
+  }
+
+  // Invalidate pointers
+  waiter->next = NULL;
+  waiter->prev = NULL;
+  waiter->in_list = false;
+}
+
+
+extern int pthread_mutex_init (pthread_mutex_t *__mutex, pthread_mutexattr_t *__mutexattr)
+{
+  memset (__mutex, 0, sizeof(*__mutex));
+  __mutexattr->name = "application_unnamed_mutex";
+  __mutexattr->cb_mem = &__mutex->_obj_mem;
+  __mutexattr->cb_size = sizeof(__mutex->_obj_mem);
+  __mutexattr->attr_bits = PTHREAD_PRIO_INHERIT;
+  __mutex->_id = osMutexNew(__mutexattr);
+  printf ("\nID: %d", __mutex->_id);
+  return 0;
+}
+
+extern int pthread_mutex_destroy (pthread_mutex_t *__mutex)
+{
+  return 0;
+}
+
+static int pthread_mutex_lock_for (pthread_mutex_t *__mutex, int millisec)
+{
+  osStatus status = osMutexAcquire(__mutex->_id, millisec);
+  return status;
+}
+
+extern int pthread_mutex_lock (pthread_mutex_t *__mutex)
+{
+  return osMutexAcquire(__mutex->_id, osWaitForever);
+}
+
+static int32_t wait (uint32_t millisec, pthread_cond_t *__restrict __cond)
+{
+  osStatus_t stat = osSemaphoreAcquire(__cond->_id, millisec);
+  switch (stat) {
+    case osOK:
+      return osSemaphoreGetCount(__cond->_id) + 1;
+    case osErrorTimeout:
+    case osErrorResource:
+      return 0;
+    case osErrorParameter:
+    default:
+      return -1;
+  }
+}
+
+extern int pthread_mutex_unlock (pthread_mutex_t *__mutex)
+{
+  return osMutexRelease(__mutex->_id);
+}
+
+
+extern int pthread_cond_init (pthread_cond_t *__restrict __cond, pthread_condattr_t *__restrict __cond_attr)
+{
+  memset(&__cond->_obj_mem, 0, sizeof(__cond->_obj_mem));
+  __cond_attr->cb_mem = &__cond->_obj_mem;
+  __cond_attr->cb_size = sizeof(__cond->_obj_mem);
+  __cond->_id = osSemaphoreNew(10, 0xffff, __cond_attr);
+  return 0;
+}
+
+extern int pthread_cond_destroy (pthread_cond_t *__cond)
+{
+  return osSemaphoreRelease(__cond->_id);
+}
+
+extern int pthread_cond_signal (pthread_cond_t *__cond)
+{
+  int ret = -1;
+
+  if (__cond->waiter._wait_list != NULL) {
+    pthread_cond_destroy(__cond->waiter._wait_list->sem);
+    _remove_wait_list(&__cond->waiter._wait_list, __cond->waiter._wait_list);
+
+    ret = 0;
+  }
+  return ret;
+}
+
+extern int pthread_cond_broadcast (pthread_cond_t *__cond)
+{
+  while (__cond->waiter._wait_list != NULL) {
+    pthread_cond_destroy(__cond->waiter._wait_list->sem);
+    _remove_wait_list(&__cond->waiter._wait_list, __cond->waiter._wait_list);
+  }
+}
+
+extern int pthread_cond_wait (pthread_cond_t *__restrict __cond, pthread_mutex_t *__restrict __mutex)
+{
+  Waiter current_thread;
+  _add_wait_list(&__cond->waiter._wait_list, &current_thread);
+
+  pthread_mutex_unlock (__mutex);
+  int32_t sem_count = wait (osWaitForever ,current_thread.sem);
+  bool timeout = (sem_count > 0) ? false : true;
+
+  pthread_mutex_lock (__mutex);
+
+
+  if (current_thread.in_list) {
+    _remove_wait_list(&__cond->waiter._wait_list, &current_thread);
+  }
+
+  return timeout;
+}
+
+static int convert_abstime_to_mili (const struct timespec *__restrict __abstime)
+{
+  int mili;
+  if (__abstime->tv_nsec)
+  {
+    mili = __abstime->tv_nsec / 1000000;
+  }
+
+  if (__abstime->tv_sec)
+  {
+    mili += 1000 * __abstime->tv_sec;
+  }
+
+  return mili;
+}
+
+extern int pthread_cond_timedwait (pthread_cond_t *__restrict __cond, pthread_mutex_t *__restrict __mutex, const struct timespec *__restrict __abstime)
+{
+  Waiter current_thread;
+  _add_wait_list(&__cond->waiter._wait_list, &current_thread);
+
+  pthread_mutex_unlock (__mutex);
+  int32_t sem_count = wait (convert_abstime_to_mili (__abstime), current_thread.sem);
+  bool timeout = (sem_count > 0) ? false : true;
+
+  pthread_mutex_lock (__mutex);
+
+  if (current_thread.in_list) {
+    _remove_wait_list(&__cond->waiter._wait_list, &current_thread);
+  }
+
+  return timeout;
+}
+
+extern int pthread_condattr_init (pthread_condattr_t *__attr)
+{
+  __attr->name = "App";
+  return 0;
+}
+
+extern int pthread_condattr_destroy (pthread_condattr_t *__attr)
+{
+  __attr = NULL;
+  return 0;
+}
+
+extern int pthread_mutexattr_setprotocol (pthread_mutexattr_t *__attr, int __protocol)
+{
+  int ret = -1;
+  if (__protocol == osMutexRecursive || __protocol == osMutexPrioInherit || __protocol == osMutexRobust)
+  {
+    __attr->attr_bits = __protocol;
+    ret = 0;
+  }
+
+  return ret;
+}
---
+++ posix/pthread-test.h
@@ -0,0 +1,61 @@
+#ifndef POSIX_PTHREAD_H
+#define POSIX_PTHREAD_H
+#define _SYS__PTHREADTYPES_H_
+#include "stdbool.h"
+#include "cmsis_os2.h"
+#include "mbed_rtos1_types.h"
+#include "mbed_rtos_storage.h"
+#include <time.h>
+#define PTHREAD_PRIO_INHERIT osMutexPrioInherit
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef osMutexAttr_t pthread_mutexattr_t;
+typedef osSemaphoreAttr_t pthread_condattr_t;
+
+typedef struct pthread_mutex_t
+{
+  osMutexId_t _id;
+  mbed_rtos_storage_mutex_t _obj_mem;
+}pthread_mutex_t;
+
+typedef struct Waiter
+{
+  osSemaphoreId_t sem;
+  pthread_mutex_t _mutex;
+  bool in_list;
+  struct Waiter *prev;
+  struct Waiter *next;
+  struct Waiter *_wait_list;
+}Waiter;
+
+typedef struct pthread_cond_t
+{
+  osSemaphoreId_t _id;
+  mbed_rtos_storage_semaphore_t _obj_mem;
+  Waiter waiter;
+}pthread_cond_t;
+
+
+extern int pthread_mutex_init (pthread_mutex_t *__mutex, pthread_mutexattr_t *__mutexattr);
+extern int pthread_mutex_destroy (pthread_mutex_t * __mutex);
+extern int pthread_mutex_lock (pthread_mutex_t * __mutex);
+extern int pthread_mutex_unlock (pthread_mutex_t * __mutex);
+
+extern int pthread_cond_init (pthread_cond_t *__restrict __cond, pthread_condattr_t *__restrict __cond_attr);
+extern int pthread_cond_destroy (pthread_cond_t *__cond);
+extern int pthread_cond_signal (pthread_cond_t *__cond);
+extern int pthread_cond_broadcast (pthread_cond_t *__cond);
+
+extern int pthread_cond_wait (pthread_cond_t *__restrict __cond, pthread_mutex_t *__restrict __mutex);
+extern int pthread_cond_timedwait (pthread_cond_t *__restrict __cond, pthread_mutex_t *__restrict __mutex, const struct timespec *__restrict __abstime);
+extern int pthread_condattr_init (pthread_condattr_t *__attr);
+extern int pthread_condattr_destroy (pthread_condattr_t *__attr);
+extern int pthread_mutexattr_setprotocol (pthread_mutexattr_t *__attr, int __protocol);
+
+#ifdef __cplusplus
+}
+#endif
+#endif //POSIX_PTHREAD_H
